d92732e869bd9ff3a09b6821a20b9423220f58fd
luizalvesr <luizalvesr@gmail.com>
2017-05-22 08:59:35 -0300

    Email

--- pmus/src/pmus/com/LoginBean.java
+++ pmus/src/pmus/com/LoginBean.java
@@ -4,9 +4,20 @@ public class LoginBean
 {
     private String name;
     private String password;
+    private String email;
 
 
-    public String getName ()
+    public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getName ()
     {
         return name;
     }
